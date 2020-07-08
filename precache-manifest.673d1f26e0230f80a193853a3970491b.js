fee4443e56757dac51beb1b4907cbda07c84b317
denkondratiev <denys.kondratiev.job@gmail.com>
2020-07-08 23:00:10 +0300

    Updates

---
+++ precache-manifest.673d1f26e0230f80a193853a3970491b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cb70592e1a6a34442dde00207169b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3627e449719a8d622417",
+    "url": "./static/css/main.4577258b.chunk.css"
+  },
+  {
+    "revision": "4af7d8882fcf58f27701",
+    "url": "./static/js/2.3b144016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3b144016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3627e449719a8d622417",
+    "url": "./static/js/main.28688df6.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
